9717ae5669549a0ed64eda208f9890238f3625ed
Garion's Blade <yuxuanng@hotmail.com>
2019-02-27 18:56:29 -0600

    overall filter

--- filtering.js
+++ filtering.js
@@ -3,55 +3,64 @@ var products = [
         id: 0,
         workerScore: 3.3,
         envScore: 3.5,
-        animalScore: 3.7
+        animalScore: 3.7,
+        overallScore: 3.5
     },
     {
         id: 1,
         workerScore: 5,
         envScore: 4.5,
-        animalScore: 5
+        animalScore: 5,
+        overallScore: 4.8
     },
     {
         id: 2,
         workerScore: 5,
         envScore: 3.5,
-        animalScore: 0.7
+        animalScore: 0.7,
+        overallScore: 3
     },
     {
         id: 3,
         workerScore: 3.3,
         envScore: 3.5,
-        animalScore: 1.7
+        animalScore: 1.7,
+        overallScore: 2.8
     },
     {
         id: 4,
         workerScore: 5,
         envScore: 2.5,
-        animalScore: 3.7
+        animalScore: 3.7,
+        overallScore: 3.7
     },
     {
         id: 5,
         workerScore: 4.3,
         envScore: 3.5,
-        animalScore: 3.7
+        animalScore: 3.7,
+        overallScore: 3.8
     },
     {
         id: 6,
         workerScore: 3.3,
         envScore: 3.5,
-        animalScore: 2.7
+        animalScore: 2.7,
+        overallScore: 3.2
     },
     {
         id: 7,
         workerScore: 0.3,
         envScore: 3.5,
-        animalScore: 3.7
+        animalScore: 3.7,
+        overallScore: 2.5
     },
     {
         id: 8,
         workerScore: 3.3,
         envScore: 4.5,
-        animalScore: 3.7
+        animalScore: 3.7,
+        overallScore: 3.8
     }
 ]
 
@@ -59,12 +68,14 @@ var items = [];
 var envFilter;
 var animalFilter;
 var workerFilter;
+var overallFilter;
 var isFilterShown;
 var filterView;
 var overallScoreBtns = [];
 var scoreContainers = [];
 
 function init(){
+  overallFilter = document.getElementById("overall-filter");
   envFilter= document.getElementById("env-filter");
   animalFilter= document.getElementById("animal-filter");
   workerFilter= document.getElementById("worker-filter");
@@ -83,9 +94,10 @@ function applyFilters() {
   var envValue = envFilter.value;
   var workerValue= workerFilter.value;
   var animalValue = animalFilter.value;
+  var overallValue = overallFilter.value;
   var filteredProducts = [];
   products.forEach(p=>{
-    if (p.workerScore>=workerValue && p.envScore>=envValue && p.animalScore>=animalValue){
+    if (p.workerScore>=workerValue && p.envScore>=envValue && p.animalScore>=animalValue && p.overallScore>=overallValue){
       filteredProducts.push(p.id);
     }
   })
@@ -97,6 +109,7 @@ function clearFilters(){
   envFilter.value=0
   workerFilter.value=0
   animalFilter.value=0
+  overallFilter.value=0
   items.forEach((i)=>i.hidden=false);
   // document.getElementById("EnValue").innerHTML="Environmental Substainability"
   // document.getElementById("AnValue").innerHTML="Animal Treatment"
@@ -104,7 +117,15 @@ function clearFilters(){
   document.getElementById("Enstars").innerHTML=""
   document.getElementById("Anstars").innerHTML=""
   document.getElementById("Workerstars").innerHTML=""
+  document.getElementById("Overallstars").innerHTML=""
 
+}
+function showvalue4(){
+  // document.getElementById("EnValue").innerHTML="Environmental Substainability--"+envFilter.value
+  document.getElementById("Overallstars").innerHTML=""
+  for (var i=0;i<overallFilter.value;i++){
+    document.getElementById("Overallstars").innerHTML+='<i class="fa fa-star" style="color:#fb0"></i>'
+  }
 }
 function showvalue1(){
   // document.getElementById("EnValue").innerHTML="Environmental Substainability--"+envFilter.value
